0698ce624408051fdce3b7a1f3a491beb4fcee67
dvdluiz <dvdluiz1991@gmail.com>
2022-02-11 19:51:24 -0300

    crud parte um

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require 'config.php';
+
+$lista = [];
+$sql = $pdo->query("SELECT * FROM usuario");
+if($sql->rowCount() > 0){
+    $lista = $sql->fetchAll(PDO::FETCH_ASSOC);
+}
 ?>
 
 <h1>Listagem de Usuários</h1>
@@ -10,7 +16,21 @@ require 'config.php';
         <th>Nome</th>
         <th>Email</th>
         <th>Ações</th>
-
     </tr>
+    <?php foreach($lista as $usuario): ?>
+        <tr>
+            <td><?=$usuario['id'];?></td>
+            <td><?=$usuario['nome'];?></td>
+            <td><?=$usuario['email'];?></td>
+            <td>
+                <a href="editar.php?id=<?=$usuario['id'];?>">[ Editar ]</a>
+                <a href="excluir.php?id=<?=$usuario['id'];?>">[ Excluir ]</a>
+
+            </td>
+
+        </tr>
+
+
+    <?php endforeach; ?>
 </table>
 <a href="cadastrar.php">Cadastrar Usuário</a>
